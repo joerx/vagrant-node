07fd0bd184745f0dce5cfe5582bf89ce5563d6ff
Joerg Henning <henning.joerg@gmail.com>
2014-03-19 21:01:15 +0700

    Swapped puppetlabs/nodejs for willdurand/nodejs, added node-inspector to stack

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,8 +1,15 @@
 
-# class {"nodejs":}
-class {"nodejs": manage_repo => true}
+class { 'nodejs':
+  version => 'v0.10.26',
+  make_install => false
+}
 
 package {"jasmine-node": 
+  provider => "npm",
+  require => Class["nodejs"]
+}
+
+package {"node-inspector":
   provider => "npm",
   require => Class["nodejs"]
 }
